5b75f1fad7542f1ecae627ccbcf5ff995074a05d
mishyta <mojpizduk@gmail.com>
2021-10-01 14:35:06 +0300

    add comments

--- Python/work with pathlib.py
+++ Python/work with pathlib.py
@@ -1,27 +1,27 @@
-from os import terminal_size
 from pathlib import Path
-from typing import Text
 
-def ls(path = Path.cwd()):
-    return [f.name for f in path.glob("*")]
+def ls(path = Path.cwd()): # how ls in linux
+    return [f.name for f in path.glob("*")] # List comprehension "the contents of the folder"
 
-print(Path.cwd())
-print(Path.home())
+print(Path.cwd()) # displays the path to the current dir
+print(Path.home()) # displays the path to home
 
-dpath = Path.cwd() / 'new_dir'
-dpath.mkdir(exist_ok=True)
+dpath = Path.cwd() / 'new_dir' # create dpath with path 'current dir/new_dir
+print(dpath)
+dpath.mkdir(exist_ok=True) # create dir 'new_dir' in current dir
 print(ls())
-dpath.rmdir()
+dpath.rmdir() # remove dir 'new_dir' in current dir
 print(ls())
 
-fpath = Path.cwd() / 'output.txt'
-fpath.touch()
+fpath = Path.cwd() / 'output.txt' # create dpath with path 'current dir/output.txt
+print(fpath)
+fpath.touch()  # create file 'new_dir' in current dir
 print(ls())
+text = fpath.read_text() # read file
+print(text)
+fpath.write_text('some text') # write to file
 text = fpath.read_text()
 print(text)
-fpath.write_text('some text')
-text = fpath.read_text()
-print(text)
-fpath.unlink()
+fpath.unlink() # delete file
 print(ls())
 
